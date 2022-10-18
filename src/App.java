a3f31e41e7c5ca179ae4517e8760630ba76f2284
Felix <felix@capchap.se>
2022-10-18 14:59:07 +0200

    Making a tamagotchi

--- src/App.java
+++ src/App.java
@@ -1,41 +1,84 @@
 // Chat bot - 1.0
 
-import java.util.Scanner;
+import java.sql.Time;
 import java.util.concurrent.TimeUnit;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String name = CollectString.main();
-        Timeout.main(1);
-        Greet.main(name);
+        world.main();
+        TimeUnit.SECONDS.sleep(5);
     }
 }
 
-class CollectString {
-    public static String main() {
-        System.out.println("What is your name?");
-        try (Scanner scanner = new Scanner(System.in)) {
-            String name = scanner.nextLine();
-            return name;
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+
+class Animal {
+    public String name;
+    public String type;
+    public String sound;
+    public int age;
+    public String color;
+
+    public Animal(String name, String type, String sound, int age, String color) {
+        this.name = name;
+        this.type = type;
+        this.sound = sound;
+        this.age = age;
+        this.color = color;
+    }
+
+    public void makeSound() {
+        System.out.println(this.sound);
+    }
+
+    public void printInfo() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Type: " + this.type);
+        System.out.println("Sound: " + this.sound);
+        System.out.println("Age: " + this.age);
+        System.out.println("Color: " + this.color);
+    }
+
+    public void printAnimal () {
+            System.out.print("\033[H\033[2J");
+            System.out.println("  __    ");
+            System.out.println(" (oo)   ");
+            System.out.println(" (__)   ");
+            System.out.println("  UU    ");
+    }
+
+    public void walkRight () {
+        System.out.print("\033[H\033[2J");
+        System.out.println("   __    ");
+        System.out.println("  (oo)   ");
+        System.out.println("  (__)   ");
+        System.out.println("   U U    ");
+    }
+
+    public void walkLeft () {
+        System.out.print("\033[H\033[2J");
+        System.out.println("  __    ");
+        System.out.println(" (oo)   ");
+        System.out.println(" (__)   ");
+        System.out.println(" U U    ");
+    }
+}
+
+class world {
+    public static void main () {
+        newWorld();
+    }
+    public static void newWorld (){
+        System.out.print("\033[H\033[2J");
+        String[] layer = {"|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"};
+        for (var i = 0; i < 4; i++) {
+            printLayerArray(layer);
         }
-        return null;
-    }
-}
 
-class Greet {
-    public static void main(String name) {
-        System.out.println("Hello, " + name);
     }
-}
-
-class Timeout {
-    public static void main(int seconds) {
-        try {
-            TimeUnit.SECONDS.sleep(seconds);
-        } catch (InterruptedException e) {
-            System.out.println("Error: " + e.getMessage());
+    public static void printLayerArray (String[] layer) {
+        for (int i = 0; i < layer.length; i++) {
+            System.out.print(layer[i]);
         }
-    }
+        System.out.println();
+    }   
 }
